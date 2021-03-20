e112af62a99e89c0d515e705c5d3b9d388e2e668
Riddhigoyal <72336061+Riddhigoyal@users.noreply.github.com>
2021-03-20 22:23:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,74 +1,192 @@
-video="";
-object=[];
-objectname=document.getElementById("box").value;
 
-function preload(){
-    
+/*created by prashant shukla */
+
+var paddle2 =10,paddle1=10;
+
+var paddle1X = 10,paddle1Height = 110;
+var paddle2Y = 685,paddle2Height = 70;
+
+var score1 = 0, score2 =0;
+var paddle1Y;
+
+var  playerscore =0;
+var audio1;
+var pcscore =0;
+//ball x and y and speedx speed y and radius
+var ball = {
+    x:350/2,
+    y:480/2,
+    r:20,
+    dx:3,
+    dy:3
 }
+
+rightX="";
+rightY="";
+rightScore="";
 
 function setup(){
-    canvas=createCanvas(480,380);
-    canvas.center();
-    video=createVideo("video.mp4");
-    video.hide();
-}
-
-function speak(){
-    var synth= window.speechSynthesis;
-    speakdata="Taking your Selfie in 5 seconds";
-    utterthis= new SpeechSynthesisUtterance(speakdata);
-    synth.speak(utterthis); 
-    setTimeout(function() {
-    }, 5000);
-}
-
-
-function start(){
-    objectdetector=ml5.objectDetector('cocossd',modelLoaded);
-    document.getElementById("status").innerHTML-"Status: Detecting Objects";
+  var canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
+  video=createCapture(VIDEO);
+  video.size(700,600);
+  video.hide();
+  posenet=ml5.poseNet(video,modelLoaded);
+  posenet.on('pose',got_results);
 }
 
 function modelLoaded(){
-    console.log ("cocossd is Initialized");
-    status=true;
-    video.loop();
-    video.speed(1);
-    video.volume(0);
+  console.log ("poseNet is Initialized");
 }
 
-function got_result(error,result){
-    if(error){
-        console.log(error);
-    }
-    else{
-        console.log(result);
-        object=result;
-    }
+function got_results(result){
+  if(result.length > 0)   {
+    rightX= result[0].pose.nose.x;
+    rightY= result[0].pose.nose.y;
+    rightScore= result[0].pose.keypoints[1].score;
+    }  
 }
-
 
 function draw(){
-    image(video,0,0,480,380);
-     
-    if(status != ""){
-        objectdetector.detect(video,got_result);
-        for(i=0 ; i<object.length ; i++){
-            document.getElementById("status").innerHTML="Object Detected";
-            document.getElementById("objects").innerHTML="Number of objects detected are " + object.length;
-            percent=floor(object[i].confidence*100);
-            fill("#FF0000");
-            textsize(20);
-            text(object[i].label + " " + percent + "%", object[i].x +15 , object[i].y +15);
-            noFill();
-            stroke("#FF0000");
-            rect(object[i].x ,object[i].y ,object[i].width ,object[i].height);
-        }
-    }
 
-    if(object[i].label == objectname){
-        document.getElementById("founding").innerHTML=objectname + "found";
-        for(i=0 ; i>=2 ; i++){
-            speak();
-        }
+  if(rightScore > 0.2){
+    fill("#000080");
+    stroke("#000080");
+    circle(rightX,rightY,30);
+  }
+
+ background(0); 
+
+ fill("black");
+ stroke("black");
+ rect(680,0,20,700);
+
+ fill("black");
+ stroke("black");
+ rect(0,0,20,700);
+ image(video,0,0,700,600)
+ 
+   //funtion paddleInCanvas call 
+   paddleInCanvas();
+ 
+   //left paddle
+   fill(250,0,0);
+    stroke(0,0,250);
+    strokeWeight(0.5);
+   paddle1Y = mouseY; 
+   rect(paddle1X,paddle1Y,paddle1,paddle1Height,100);
+   
+   
+    //pc computer paddle
+    fill("#FFA500");
+    stroke("#FFA500");
+   var paddle2y =ball.y-paddle2Height/2;  rect(paddle2Y,paddle2y,paddle2,paddle2Height,100);
+    
+    //function midline call
+    midline();
+    
+    //funtion drawScore call 
+   drawScore();
+   
+   //function models call  
+   models();
+   
+   //function move call which in very important
+    move();
+}
+
+
+
+//function reset when ball does notcame in the contact of padde
+function reset(){
+   ball.x = width/2+100,
+   ball.y = height/2+100;
+   ball.dx=3;
+   ball.dy =3;
+   
+}
+
+
+//function midline draw a line in center
+function midline(){
+    for(i=0;i<480;i+=10) {
+    var y = 0;
+    fill("white");
+    stroke(0);
+    rect(width/2,y+i,10,480);
     }
+}
+
+
+//function drawScore show scores
+function drawScore(){
+    textAlign(CENTER);
+    textSize(20);
+    fill("white");
+    stroke(250,0,0)
+    text("Player:",100,50)
+    text(playerscore,140,50);
+    text("Computer:",500,50)
+    text(pcscore,555,50)
+}
+
+
+//very important function of this game
+function move(){
+   fill(50,350,0);
+   stroke(255,0,0);
+   strokeWeight(0.5);
+   ellipse(ball.x,ball.y,ball.r,20)
+   ball.x = ball.x + ball.dx;
+   ball.y = ball.y + ball.dy;
+   if(ball.x+ball.r>width-ball.r/2){
+       ball.dx=-ball.dx-0.5;       
+   }
+  if (ball.x-2.5*ball.r/2< 0){
+  if (ball.y >= paddle1Y&& ball.y <= paddle1Y + paddle1Height) {
+    ball.dx = -ball.dx+0.5; 
+  }
+  else{
+    pcscore++;
+    reset();
+    navigator.vibrate(100);
+  }
+}
+if(pcscore ==4){
+    fill("#FFA500");
+    stroke(0)
+    rect(0,0,width,height-1);
+    fill("white");
+    stroke("white");
+    textSize(25)
+    text("Game Over!☹☹",width/2,height/2);
+    text("Reload The Page!",width/2,height/2+30)
+    noLoop();
+    pcscore = 0;
+}
+   if(ball.y+ball.r > height || ball.y-ball.r <0){
+       ball.dy =- ball.dy;
+   }   
+}
+
+
+//width height of canvas speed of ball 
+function models(){
+    textSize(18);
+    fill(255);
+    noStroke();
+    text("Width:"+width,135,15);
+    text("Speed:"+abs(ball.dx),50,15);
+    text("Height:"+height,235,15)
+}
+
+
+//this function help to not go te paddle out of canvas
+function paddleInCanvas(){
+  if(mouseY+paddle1Height > height){
+    mouseY=height-paddle1Height;
+  }
+  if(mouseY < 0){
+    mouseY =0;
+  }  
 }
